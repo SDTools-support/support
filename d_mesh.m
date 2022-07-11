709750bc2b56454f6df570c8d1e904025377ec25
vermot <vermot@sdtools.com>
2022-07-11 11:57:21 +0200

    auto nmap

--- d_mesh.m
+++ d_mesh.m
@@ -1871,6 +1871,8 @@ if js<=length(S); %  'd_hbm(Mesh0D):d_hbm(NL0Dm1t)' % sdtweb d_hbm NL
 end
 
 if ~isfield(mo1,'name');mo1.name=RO.name;end
+if isfield(RO,'nmap'); mo1.nmap=RO.nmap; sdtw('_ewt','xxxeb xxxgv need to clarify nmap recovery')
+end % sdtw('xxxeb xxxgv need to clarify
 if isfield(RO,'nmap')&&nargout==0
     RO.nmap('CurModel')=mo1; return
 end
